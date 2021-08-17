199d628dc4939a1ed0d7c26e406b464e79e168c8
Michael Rose <michael@rosenetwork.net>
2021-08-17 15:55:57 -0700

    misc

--- src/assembler/clj.clj
+++ src/assembler/clj.clj
@@ -4,8 +4,11 @@
 			)
   (:gen-class))
 
+
+
 (def label-map (atom {}))
-(def counter (atom 0))
+(def variable-map (atom {}))
+(def counter (atom -1))
 (def variable-allocation-counter (atom 15))
 
 (defn third [c]
@@ -91,7 +94,12 @@
 
 (defn build-label-map-and-preprocess [parse-tree]
   (reset! label-map {})
-  (reset! counter 0)
+  (reset! variable-map {"R0" 0 "R1" 1 "R2" 2 "R3" 3 "R4" 4 "R5" 5 "R6" 6 "R7" 7 "R8" 8
+					"R9" 9 "R10" 10 "R11" 11 "R12" 12 "R13" 13 "R14" 14 "R15" 15
+					"SCREEN" 16384
+					"KBD" 24576
+					})
+  (reset! counter -1)
   (reset! variable-allocation-counter 15)
   (insta/transform {
 		:INSTRUCTION (fn [x] (swap! counter inc) x)
@@ -120,6 +128,14 @@
    (build-label-map-and-preprocess)
    (filter identity)
    (transform-nodes)
-   ;; (str/join "\n")
-   ;; (spit "out")
+   (str/join "\n")
+   (spit "out.hack")
    )
+
+;; (->>(parse-file  "/usr/home/michael/proj/nand2tetris/projects/04/Mult.asm")
+;;    (build-label-map-and-preprocess)
+;;    (filter identity)
+;;    (transform-nodes)
+;;    ;; (str/join "\n")
+;;    ;; (spit "out.hack")
+;;    )
